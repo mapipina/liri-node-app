a424c3db273c45cb122af915ad390fe33e1905e8
Mapi Pina <mpina09@gmail.com>
2018-03-31 09:42:30 -0400

    continued following hw steps

---
+++ liri.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+var keys = require('keys');
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+* `my-tweets`
+
+* `spotify-this-song`
+
+* `movie-this`
+
+* `do-what-it-says`
+
