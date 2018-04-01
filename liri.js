6c7a9ed244c3b5580e000410e02465e237fef13b
Mapi Pina <mpina09@gmail.com>
2018-04-01 12:14:29 -0400

    twt up and running. working on spotify next

--- liri.js
+++ liri.js
@@ -1,20 +1,46 @@
-var dotenv = require('dotenv').config();
-var keys = require('./keys');
-var request = require('request');
-var Spotify = require('node-spotify-api');
-var Twitter = require('twitter');
+//Stating the required packages as dependencies
+const dotenv = require('dotenv').config();
+const keys = require('./keys');
+const request = require('request');
+const Spotify = require('node-spotify-api');
+const Twitter = require('twitter');
 
+//Storing access tokens and keys in constants
 const spotify = new Spotify(keys.spotify);
 const client = new Twitter(keys.twitter);
 
+//Grabbing commands 
 var arg = process.argv[2];
 
-if (arg === 'spotify-this-song') {
-	spotify.search({type:'track', query:'Californication', limit: 1}, function(err, data){
-		if (err) {
-			return console.log('Error occurred: ' + err);
-		}
-		console.log(JSON.stringify(data, null, 2));
-	});
-} 
+// Twitter
+//setting the parameters for the response
+const params = {
+	screen_name: '1_mpina09',
+	count: 20,
+};
+//grabbing Tweets from Twitter API
+var tweets = function(){
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!!error) {
+    console.log(error);
+  } else {
+  	for (var i = 0; i < tweets.length; i++) {
+  		console.log(tweets[i].created_at + " " + tweets[i].text)
+  	};
+  }
+});
+};
+//my-tweets
+if (arg === 'my-tweets') {
+	tweets();
+};
+
+// if (arg === 'spotify-this-song') {
+// 	spotify.search({type:'track', query:'Californication', limit: 1}, function(err, data){
+// 		if (err) {
+// 			return console.log('Error occurred: ' + err);
+// 		}
+// 		console.log(JSON.stringify(data, null, 2));
+// 	});
+// } 
 
