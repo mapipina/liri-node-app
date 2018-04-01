53ee80a1fd6a8683f6bd89e2b8225c79826884a0
Mapi Pina <mpina09@gmail.com>
2018-03-31 20:22:33 -0400

    adding some more requires  starting coding

--- liri.js
+++ liri.js
@@ -1,14 +1,20 @@
-require("dotenv").config();
-var keys = require('keys');
+var dotenv = require('dotenv').config();
+var keys = require('./keys');
+var request = require('request');
+var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+const spotify = new Spotify(keys.spotify);
+const client = new Twitter(keys.twitter);
 
-* `my-tweets`
+var arg = process.argv[2];
 
-* `spotify-this-song`
-
-* `movie-this`
-
-* `do-what-it-says`
+if (arg === 'spotify-this-song') {
+	spotify.search({type:'track', query:'Californication', limit: 1}, function(err, data){
+		if (err) {
+			return console.log('Error occurred: ' + err);
+		}
+		console.log(JSON.stringify(data, null, 2));
+	});
+} 
 
