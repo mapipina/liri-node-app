2b8efb60d05db4354d80abed4fbc911ccb757fd8
Mapi Pina <mpina09@gmail.com>
2018-04-05 17:40:27 -0400

    got the do what it says working

--- liri.js
+++ liri.js
@@ -4,6 +4,7 @@ const keys = require('./keys');
 const request = require('request');
 const Spotify = require('node-spotify-api');
 const Twitter = require('twitter');
+const fs = require('file-system');
 
 //Storing access tokens and keys in constants
 const spotify = new Spotify(keys.spotify);
@@ -41,22 +42,90 @@ for (var i = 2; i < process.argv.length; i++) {
   q = q + " " + process.argv[i];
 };
 // if statement for 
-if (arg === 'spotify-this-song' && q !== null) {
-} else {
-  q = 'I Want it That Way';
-};
+// if (arg === 'spotify-this-song' && q !== null) {
+//   songs();
+// } else {
+//   q = 'I Want it That Way';
+// };
 
 var songs = function() {spotify.search({type: 'track', query: q, limit: 1}, function(err, data)
-  {if(err){
-    console.log("this error is " + err)
+  {if(arg === 'spotify-this-song' && q !== null){
+    console.log(data.tracks.items[0].album.artists[0].name); 
+    console.log(data.tracks.items[0].name);
+    console.log(data.tracks.items[0].album.name);
+    console.log(data.tracks.items[0].external_urls.spotify);c
+    } else {
+      q = 'I Want it That Way';
     };
-  console.log(data.tracks.items[0].album.artists[0].name); 
-  console.log(data.tracks.items[0].name);
-  console.log(data.tracks.items[0].album.name);
-  console.log(data.tracks.items[0].external_urls.spotify);
-  })
+  });
 };
 
-songs();
+// OMBD portion
+var movie ='';
+for (var i = 2; i < process.argv.length; i++) {
+  movie = movie + " " + process.argv[i];
+};
 
+request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", function(error, response, body) {
 
+  // If there were no errors and the response code was 200 (i.e. the request was successful)...
+  if (!error && response.statusCode === 200) {
+    // Then we print out the imdbRating
+    console.log("Title: " + JSON.parse(body).Title);
+    console.log("Year Released: " + JSON.parse(body).Released);
+    console.log("imdbRating: " + JSON.parse(body).imdbRating);
+    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).imdbRating);
+    console.log("Country: " + JSON.parse(body).imdbRating);
+    console.log("Language: " + JSON.parse(body).imdbRating);
+    console.log("Plot: " + JSON.parse(body).imdbRating);
+    console.log("Cast: " + JSON.parse(body).imdbRating);
+  }
+});
+
+if (arg === 'movie-this') {
+
+}
+
+// lookUpMovie: (movie) => {
+//         var options = {
+//             uri: ‘http://www.omdbapi.com/',
+//             qs: {
+//                 apikey: keys.omdb.key, // -> uri + ‘?access_token=xxxxx%20xxxxx’
+//                 t: movie
+//             },
+//             headers: {
+//                 ‘User-Agent’: ‘Request-Promise’
+//             },
+//             json: true // Automatically parses the JSON string in the response
+//         };
+//         rp(options)
+//         .then((body) => {
+//           console.log(`# Title: ${body.Title}`);
+//           console.log(`# Year: ${body.Released}`);
+//           console.log(`# IMDB Rating: ${body.Ratings[0].Value}`);
+//           console.log(`# Rotten Tomatoes Rating: ${body.Ratings[1].Value}`);
+//           console.log(`# Country: ${body.Country}`);
+//           console.log(`# Language: ${body.Language}`);
+//           console.log(`# Plot: ${body.Plot}`);
+//           console.log(`# Cast: ${body.Actors}`);
+//         })
+//         .catch(error => {
+//             console.log("error:", error); 
+//         });
+//     };
+
+// Do what it says portion
+function doit () {
+  fs.readFile("./random.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  // console.log(data);
+  const lines = data.split(',')
+  for (var i = 0; i < lines.length; i++) {
+      console.log(lines[i])
+  };
+});
+}
+
+if (arg === 'do-what-it-says') {
+  doit();
+};
